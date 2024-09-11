b66dd24179171747b66d5f3e9c964262cf3c14bb
mohdamir021 <Emperor%2354>
2024-09-11 17:05:03 +0800

    feat(page): update index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,22 @@
-export default function Home() {
+import LoginBody from "@/components/ui/body/LoginBody";
+import Header from "@/components/ui/texts/Header";
+import { Flex } from "@chakra-ui/react";
+
+export default function Login() {
   return (
-    <>Home Page</>
+    <Flex w={"full"} h={"100vh"} bgColor={"lightblue"} justifyContent={"end"}>
+      <Flex
+        w={"50%"}
+        bgColor={"whitesmoke"}
+        h={"full"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        flexDir="column"
+        gap={"20px"}
+      >
+        <Header>Social Media</Header>
+        <LoginBody />
+      </Flex>
+    </Flex>
   );
 }
